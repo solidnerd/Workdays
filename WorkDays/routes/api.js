0a387556e436a037c6f120f5bdddc46008420b30
SolidNerd <niclas@mietz.io>
2015-07-25 14:38:30 +0200

    Add Date Api

---
+++ WorkDays/routes/api.js
@@ -0,0 +1,21 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/dates', function (req, res) {
+  res.json(
+    {
+      "success": 1,
+      "result": [{
+          "id": 1,
+          "title": "Arbeiten",
+          "url": "http://example.com",
+          "class": "event-important",
+          "start": 1437826249000, // Milliseconds
+          "end": 1437829849000 // Milliseconds
+        }]
+    }
+   );
+});
+
+module.exports = router;
