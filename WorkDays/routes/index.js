ee67ce28171e8f4dc4658df1acae4a79af536775
SolidNerd <niclas@mietz.io>
2015-07-26 19:31:17 +0200

    Startpage is now more configureable

--- WorkDays/routes/index.js
+++ WorkDays/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', { user: 'nm', dates: '/api/dates' });
+  res.render('index', { user: process.env.User || 'Unkown', workplace: process.env.Workplace || 'Workplace', dates: '/api/dates' });
 });
 
 module.exports = router;
