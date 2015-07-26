3784916d80226a555006a4c7efb26f25e754a3c1
SolidNerd <niclas@mietz.io>
2015-07-26 19:33:27 +0200

    Api uses now mysql

--- WorkDays/routes/api.js
+++ WorkDays/routes/api.js
@@ -1,21 +1,44 @@
 ﻿var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 /* GET users listing. */
 router.get('/dates', function (req, res) {
-  res.json(
-    {
-      "success": 1,
-      "result": [{
-          "id": 1,
-          "title": "Arbeiten",
+  var resultArray = new Array();
+  var connection = mysql.createConnection( {
+    host     : process.env.MYSQL_HOST || 'localhost',
+    port     : process.env.MYSQL_PORT || '3306',
+    user     : process.env.MYSQL_USER || 'mysql',
+    password : process.env.MYSQL_USER_PASSWORD  || 'mysql',
+  });
+  var calendarid = 2;
+  var columns = ['summary', 'startdate', 'enddate']
+  var query = connection.query('SELECT ?? FROM ?? WHERE calendarid = ?', [columns, 'owncloud.oc_clndr_objects', calendarid], function (err, results) {
+    connection.end();
+    var id = 0;
+    if (!err){
+      results.forEach(function (entry) {
+        id++;
+        resultArray.push({
+          "id": id,
+          "title": entry.summary,
           "url": "http://example.com",
           "class": "event-important",
-          "start": 1437826249000, // Milliseconds
-          "end": 1437829849000 // Milliseconds
-        }]
+          "start": entry.startdate.getTime(),// Milliseconds
+          "end": entry.enddate.getTime() // Milliseconds
+        });
+      });
+      res.json({
+        "success": 1,
+        "result": resultArray
+      });
     }
-   );
+    else {
+      console.log('Error while performing Query.');
+      res.json({ success: 0, error: 'Something terrible happened' });
+    }
+  });
+
 });
 
 module.exports = router;
